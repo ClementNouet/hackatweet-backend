e219c27bfbd2c0282d5dbb53e0d14a46a67f6cdc
Diego <gomeztorres.diegoandres@gmail.com>
2024-11-21 14:41:19 +0100

    routes tweets

--- models/tweets.js
+++ models/tweets.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const tweetSchema = mongoose.Schema({
     content: String,
     createAt: Date,
-    users: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
    });
    
    const Tweet = mongoose.model('tweets', tweetSchema);
--- routes/tweets.js
+++ routes/tweets.js
@@ -6,38 +6,30 @@ require('../models/connection');
 const User = require('../models/users');
 const Tweet = require('../models/tweets');
 
-router.post('/newTweet/:token', (req, res) => {
-
-    // Trouver l'utilisateur par son token depuis les paramètres d'URL
-    User.findOne({ token: req.params.token }).then(user => {
-      if (!user) {
-        res.json({ result: false, error: 'User not found' });
-        return;
-      }
-
-    const newTweet = new Tweet({
-      content: req.body.content, 
-      createAt: new Date,
-      users: user._id 
-    });
-
-    newTweet.save().then(tweet => {
-      res.json({ result: true, tweet });
-    })
+//Recouperer tous les tweets
+router.get('/', function(req, res, next) {
+  Tweet.find({}).populate('user').then(data => {
+    res.json({ content: data.content, createAt: data.createAt, token: data.user.token });
   })
-});
-
-// Route DELETE qui permet de supprimer le voyage concerné et de renvoyer les voyage contenu dans le cart
-router.delete("/:id", (req, res) => {
-    Tweet.deleteOne({ _id: req.params.id }).then((deleteTweet) => {
-      if (deleteTweet.deletedCount > 0) {
-        Tweet.find().then((data) => res.json({ result: true, tweet: data }));
-        console.log("Tweet successfully deleted");
-      } else {
-        res.json({ result: false, error: "No tweet deleted" });
-      }
-    });
-  });
+})
+//Créer un tweet
+router.post('/NewTweet/:token', (req, res) => {
+  User.findOne({token: req.params.token}).then(data => {
+    const newTweet = new Tweet({
+      content: req.body.tweet,
+      createAt: new Date,
+      user: data._id
+    })
+    newTweet.save().then(res.json({ result: true }))
+  })
+}) 
+//Effacer un tweet 
+router.delete('/:id', function(req, res, next) {
+  const id = req.params.id
+  Tweet.deleteOne({ _id: id }).then(()=>{
+      res.json({ message: "This tweet has been deleted."})
+  })
+})
 
 
 module.exports = router;
